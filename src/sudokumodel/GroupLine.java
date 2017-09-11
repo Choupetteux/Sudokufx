f3c6fdc09c2ae1934d8d310fc14c31f46c8d901b
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-09-11 12:13:27 +0200

    TODO : GroupLine, GroupColumn, GroupBloc, etc.

---
+++ src/sudokumodel/GroupLine.java
@@ -0,0 +1,10 @@
+package sudokumodel;
+
+public class GroupLine extends Group{
+
+	public GroupLine(int groupNumber, Sudoku sudoku) {
+		super(groupNumber, sudoku);
+		// TODO Auto-generated constructor stub
+	}
+
+}
