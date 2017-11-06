b740408af857cfa6154ebaa2ca121a9d089ad000
Luigi Ribbi <luigi.ribbi@etudiant.univ-reims.fr>
2017-11-06 11:22:47 +0100

    Résolution d'erreur, rework GroupBloc

--- src/sudokumodel/GroupBloc.java
+++ src/sudokumodel/GroupBloc.java
@@ -4,9 +4,20 @@ public class GroupBloc extends Group{
 
 	public GroupBloc(int groupNumber, Sudoku sudoku) {
 		super(groupNumber, sudoku);
+String s = "" + groupNumber + "  _ ";
 		for(int i = 0; i<9;i++){
-			this.cells[i] = ((this.getGroupNumber() % 3) * 3) + (((this.getGroupNumber() / 3) * 3)*9);
+			if(i<3){
+				this.cells[i] = ((this.getGroupNumber() % 3) * 3) + (((this.getGroupNumber() / 3) * 3)*9) + i;
+			}
+			else if(i>=3 && i<=5){
+				this.cells[i] = ((this.getGroupNumber() % 3) * 3) + (((this.getGroupNumber() / 3) * 3)*9) + 9 + i-3;
+			}
+			else if(i>=6){
+				this.cells[i] = ((this.getGroupNumber() % 3) * 3) + (((this.getGroupNumber() / 3) * 3)*9) + 18 + i-6;
+			}
+			s += "  " +i+":"+this.cells[i]; 
 		}
+//System.out.println(s);		
 	}
 
 }
